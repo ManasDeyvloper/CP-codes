275fca23f53c79f7b50bbd761605954de83dd893
ManasDey <deyvloper1003@gmail.com>
2023-02-05 11:24:32 +0530

    Create codecheffavgproblem.cpp

---
+++ codecheffavgproblem.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int t;
+ cin>>t;
+ while(t--){
+     int a,b,c;
+     cin>>x>>y>>z;
+     if((a+b)/2 < c)
+     {
+        cout<<YES<<"\n";
+         
+     }
+    
+     else 
+     {
+         cout<<NO<<"\n";
+     }
+ }
+}
