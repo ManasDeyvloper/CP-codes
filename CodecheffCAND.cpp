abdb21721d695046ab5d31b2300f9e595aa3c480
ManasDey <deyvloper1003@gmail.com>
2023-02-05 13:32:56 +0530

    more codes

---
+++ CodecheffCAND.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int t;
+ cin>>t;
+ while(t--)
+ {
+    int n,x,i;
+    cin>>n>>x;
+    while(x<n)
+    { 
+        x= x+4;
+        i++;
+
+
+    }
+    cout<<i;
+    
+    
+ }
+}
---
+++ truescore.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int t;
+ cin>>t;
+ while(t--)
+ {
+    int a,b,c,d;
+    cin>>a>>b;
+    cin>>c>>d;
+    if(c>=a && d>=b)
+    {
+        cout<<"possible"<<"\n";
+
+    }
+    else
+    {
+        cout<<"Impossible"<<"\n";
+    }
+
+    
+    
+    
+ }
+}
